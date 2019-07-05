7ee3c917ba6625bcee0803d8aecbbb8bc502e71b
ondras12345 <ondras12345@gmail.com>
2019-07-05 22:36:54 +0200

    Correct code formating
    * Add shebang line
    * Remove unnecessary import
    * Correct comment style

--- B35T/B35T.py
+++ B35T/B35T.py
@@ -1,39 +1,39 @@
-#https://github.com/gregpinero/ArduinoPlot/blob/master/Arduino_Monitor.py
-#https://code-maven.com/interactive-shell-with-cmd-in-python
+#!/usr/bin/env python
+# https://github.com/gregpinero/ArduinoPlot/blob/master/Arduino_Monitor.py
+# https://code-maven.com/interactive-shell-with-cmd-in-python
 
-#TODO - timeouts
-#TODO - test delta measurement with all ranges
-#TODO - buttons - try https://github.com/reaper7/M5Stack_BLE_client_Owon_B35T/blob/master/M5Stack_BLE_client_Owon_B35T.ino
+# TODO - timeouts
+# TODO - test delta measurement with all ranges
+# TODO - buttons - try https://github.com/reaper7/M5Stack_BLE_client_Owon_B35T/blob/master/M5Stack_BLE_client_Owon_B35T.ino
 
 import serial
 import threading
 import datetime
 import operator
-import sys
 import logging as log
 
 
 DATA_LENGTH = 14
-ABSOLUTE_ERROR = 5 #max least significant digit deviation
-RELATIVE_ERROR = 10 #in %, max difference between two measurements
+ABSOLUTE_ERROR = 5  # max least significant digit deviation
+RELATIVE_ERROR = 10  # in %, max difference between two measurements
 
 
-received_data = [] #global variable for transfering the from serial_thread
+received_data = []  # global variable for transfering the from serial_thread
 
 ###############################################################################################################################
 #                                                       WARNINGS                                                              #
 #                                                                                                                             #
 ###############################################################################################################################
-#0.001 does NOT equal 10 ** (-3) (python2 only) --> all unit prefixes must be in scientific format (10e(n))
+# 0.001 does NOT equal 10 ** (-3) (python2 only) --> all unit prefixes must be in scientific format (10e(n))
 
-#Mode 48 - occurs when rotating the switch - handled by try...except in the serial thread
+# Mode 48 - occurs when rotating the switch - handled by try...except in the serial thread
 
-#Python 3 handles strings differenty (The string type in Python 2 is a list of 8-bit characters, 
-#but the bytes type in Python 3 is a list of 8-bit integers.
-#http://python3porting.com/problems.html) --> removed ord(), use bytearray()
+# Python 3 handles strings differenty (The string type in Python 2 is a list of 8-bit characters,
+# but the bytes type in Python 3 is a list of 8-bit integers.
+# http://python3porting.com/problems.html) --> removed ord(), use bytearray()
 
-#Amps range does contain 00 00 (00 and units),
-#so I have to check for more 00 bytes in a sequence)
+# Amps range does contain 00 00 (00 and units),
+# so I have to check for more 00 bytes in a sequence)
 
 
 ###############################################################################################################################
@@ -41,31 +41,34 @@ received_data = [] #global variable for transfering the from serial_thread
 #                                                                                                                             #
 ###############################################################################################################################
 class B35T_MeasuredValue(object):
-    #digits,
-    #units,
-    #mode,
-    #dateTime
-    
+    # digits,
+    # units,
+    # mode,
+    # dateTime
+
     def __init__(self, dateTime, digits, units, mode, LSD_position):
         self.digits = digits
         self.units = units
         self.mode = mode
         self.dateTime = dateTime
-        self.LSD_position = LSD_position #position of the least significant digit (e.g. 0.1, 0.01, ...)
-        
+        self.LSD_position = LSD_position  # position of the least significant digit (e.g. 0.1, 0.01, ...)
+
     def matches(self, B):
         log.info('Entered matches')
         log.debug('matches - Comparing {} to {}'.format(str(self), str(B)))
-        if not self.mode == B.mode: return(False)
+        if not self.mode == B.mode:
+            return(False)
 
-        valA = round(self.digits * self.units.prefix, 12) #round because 1986 * 0.1 = 198.60000000000002
+        valA = round(self.digits * self.units.prefix, 12)  # round because 1986 * 0.1 = 198.60000000000002
         valB = round(B.digits * B.units.prefix, 12)
-        
-        if valA == 0: #to prevent division by zero
-            if valB == 0 : difference = 0
-            else : difference = 99999 #to fail in the % condition
+
+        if valA == 0:  # to prevent division by zero
+            if valB == 0:
+                difference = 0
+            else:
+                difference = 99999  # to fail in the % condition
         else:
-            difference = abs((valB - valA) / valA * 100) #in %, relative to A
+            difference = abs((valB - valA) / valA * 100)  # in %, relative to A
         if difference > RELATIVE_ERROR:
             log.debug('matches - % difference too high ({}% > {}%)'.format(difference, RELATIVE_ERROR))
             absolute_difference = abs(valA - valB)
@@ -74,52 +77,54 @@ class B35T_MeasuredValue(object):
                 return(False)
         log.debug('matches - Returning True')
         return(True)
-                            
+
     def __str__(self):
-        return ('{};{};{};{}'.format(self.dateTime, self.digits, self.units, self.mode))  
-        
+        return ('{};{};{};{}'.format(self.dateTime, self.digits, self.units, self.mode))
+
     def __repr__(self):
         return ('{}({}, {}, {}, {})'.format(self.__class__.__name__, repr(self.dateTime), repr(self.digits), repr(self.units), repr(self.mode), repr(self.LSD_position)))
-               
+
+
 class B35T_Unit(object):
-    #_prefix,
-    #unitStr
-    
-    
+    # _prefix,
+    # unitStr
+
     def __init__(self, prefix, unitStr):
         self.prefix = prefix
         self.unitStr = unitStr
-    
+
     def prefixStr(self, prefix):
         prefixDict = {
-        1e-9: 'n',
-        1e-6: 'u',
-        1e-3: 'm',
-        1: '',
-        1e3: 'k',
-        1e6: 'M',
+            1e-9: 'n',
+            1e-6: 'u',
+            1e-3: 'm',
+            1: '',
+            1e3: 'k',
+            1e6: 'M',
         }
         return(prefixDict.get(prefix, 'BAD'))
-        
-    prefix = property(operator.attrgetter('_prefix')) #validation
+
+    prefix = property(operator.attrgetter('_prefix'))  # validation
     @prefix.setter
     def prefix(self, p):
-        if (self.prefixStr(p) == 'BAD'): raise Exception("Invalid prefix: {}".format(p))
-        self._prefix = p    
-            
+        if (self.prefixStr(p) == 'BAD'):
+            raise Exception("Invalid prefix: {}".format(p))
+        self._prefix = p
+
     def __str__(self):
         return('{}{}'.format(self.prefixStr(self.prefix), self.unitStr))
-        
+
     def __repr__(self):
         return ('{}({}, {})'.format(self.__class__.__name__, repr(self.prefix), repr(self.unitStr)))
-        
+
+
 class serial_thread(threading.Thread):
     def __init__(self, ser):
         threading.Thread.__init__(self)
         self.stop_event = threading.Event()
         self.ser = ser
         self.alive = True
-        
+
     def run(self):
         global received_data
         recv_data = bytearray('', 'ascii')
@@ -127,17 +132,18 @@ class serial_thread(threading.Thread):
         self._ser_sync()
         while not self.stop_event.is_set():
             log.debug('serial_thread - Waiting')
-            while not self.ser.inWaiting() >= DATA_LENGTH : pass #wait for the data
+            while not self.ser.inWaiting() >= DATA_LENGTH:
+                pass  # wait for the data
             log.debug('serial_thread - Receiving')
             recv_data = self.ser.read(DATA_LENGTH)
             try:
                 log.debug('serial_thread - Received: {}'.format(recv_data))
-                received_data.append(self._getValue(bytearray(recv_data))) #list.append() is thread safe https://stackoverflow.com/questions/6319207/are-lists-thread-safe
-                log.info('serial_thread - Added value: {}'.format(str(received_data[-1]))) #nothing else is writing to this variable
+                received_data.append(self._getValue(bytearray(recv_data)))  # list.append() is thread safe https://stackoverflow.com/questions/6319207/are-lists-thread-safe
+                log.info('serial_thread - Added value: {}'.format(str(received_data[-1])))  # nothing else is writing to this variable
             except Exception as e:
                 log.error('serial_thread - Exception {} occured.'.format(str(e)))
-                self._ser_sync() #resynchronize
-                #raise #cannot re-raise the same exception because it wouldn't be handled
+                self._ser_sync()  # resynchronize
+                # raise  # cannot re-raise the same exception because it wouldn't be handled
         log.info('serial_thread - Thread killed')
         self.alive = False
 
@@ -149,13 +155,13 @@ class serial_thread(threading.Thread):
         a = bytearray('abcd', 'ascii')
         while skip >= 0:
             if self.ser.inWaiting() > 0:
-                for i in range(3): #shift the array
+                for i in range(3):  # shift the array
                     a[i] = a[i + 1]
                 a[3] = ord(self.ser.read(1))
                 log.debug('_ser_sync - a = {}'.format(repr(a)))
 
-                if a == bytearray([0,0,0,0]): #drop the first few messages (zeros, DMM ID and junk) (see warnings)
-                    skip = 4 #skip the next 5 messages (including the zeros)
+                if a == bytearray([0, 0, 0, 0]):  # drop the first few messages (zeros, DMM ID and junk) (see warnings)
+                    skip = 4  # skip the next 5 messages (including the zeros)
                     log.debug('_ser_sync - zeros')
 
                 if a[-2:] == bytearray('\r\n', 'ascii'):
@@ -168,9 +174,8 @@ class serial_thread(threading.Thread):
         units = self._unitsObj(raw_data[9:11])
         mode = self._modeStr(raw_data[7])
         return (B35T_MeasuredValue(datetime.datetime.now(), digits, units, mode, LSD_position))
-    
-    
-    def _unitsObj(self, units): 
+
+    def _unitsObj(self, units):
         '''Returns Unit eg. mV = Unit(0.001, 'V')'''
         unitsDict = {
             (64, 128): (1e-3, 'V'),
@@ -187,37 +192,38 @@ class serial_thread(threading.Thread):
             (2, 0): (1, '%'),
             (0, 16): (1, 'hFE'),
             (4, 128): (1, 'V-diode'),
-            (0, 4): (1e-9,'F'),
+            (0, 4): (1e-9, 'F'),
             (128, 4): (1e-6, 'F'),
             (8, 32): (1, 'Ohm-continuity'),
         }
         (prefix, unit) = unitsDict.get((units[0], units[1]), (0, 0))
         if prefix == 0 and unit == 0:
             raise Exception('<unknown unit {} {}>'.format(repr(units[0]), repr(units[1])))
-     
+
         return(B35T_Unit(prefix, unit))
 
     def _modeStr(self, mode):
         '''Returns string representing the current mode'''
         modeDict = {
-            0: '', #DUTY, hFE, temperature, V-diode
-            1: '(Ohm-manual)', #manual ranging + continuity
+            0: '',  # DUTY, hFE, temperature, V-diode
+            1: '(Ohm-manual)',  # manual ranging + continuity
             8: '(AC-minmax)',
             9: '(AC-manual)',
-            12: '(AC delta)', #may be something else, but it is present when AC delta
+            12: '(AC delta)',  # may be something else, but it is present when AC delta
             16: '(DC-minmax)',
             17: '(DC-manual)',
-            20: '(delta)', #may be wrong
-            #21 - when switching to delta - did not occur during debugging
-            32: '', #Hz, F
+            20: '(delta)',  # may be wrong
+            # 21 - when switching to delta - did not occur during debugging
+            32: '',  # Hz, F
             33: '(Ohm-auto)',
             41: '(AC-auto)',
-            #48 I think it occurs when rotating the range switch
+            # 48 I think it occurs when rotating the range switch
             49: '(DC-auto)',
             51: '[HOLD]',
         }
         modeS = modeDict.get(mode, 'BAD')
-        if modeS == 'BAD': raise Exception('<unknown mode {}>'.format(repr(mode)))
+        if modeS == 'BAD':
+            raise Exception('<unknown mode {}>'.format(repr(mode)))
         return(modeS)
 
     def _digitsFloat(self, sign_digits_str, decimal_position):
@@ -230,38 +236,41 @@ class serial_thread(threading.Thread):
             52: 0.1,
         }
         if sign_digits_str[1] == ord('?') and sign_digits_str[4] == ord('?'):
-          result = 99999 #O.L
-        else: 
+            result = 99999  # O.L
+        else:
             try:
                 result = int(sign_digits_str)
             except Exception as e:
                 log.error('_digitsFloat - Exception {} occured.'.format(str(e)))
-                log.info('_digitsFloat - Exception - Data: sign_digits_str: {}, decimalpos: {}'.format(sign_digits_str, decimal_position))
+                log.info('_digitsFloat - Exception - sign_digits_str: {}, decimalpos: {}'.format(sign_digits_str, decimal_position))
                 raise Exception('Could not convert to int: {}'.format(sign_digits_str))
-        
+
         coef = coefDict.get(decimal_position, 'BAD')
         log.debug('_digitsFloat - coef: {}, result: {}'.format(coef, result))
-        if coef != 'BAD': result *= coef
-        else: raise Exception('Could not get coefficient: {}'.format(repr(decimal_position)))
-        result = round(result, 4) #to remove floating point operations least significant digit junk
+        if coef != 'BAD':
+            result *= coef
+        else:
+            raise Exception('Could not get coefficient: {}'.format(repr(decimal_position)))
+        result = round(result, 4)  # to remove floating point operations least significant digit junk
         log.debug('_digitsFloat - returning ({}, {})'.format(result, coef))
         return((result, coef))
-    
+
+
 ###############################################################################################################################
 #                                                      B35T class                                                             #
 #                                                                                                                             #
 ###############################################################################################################################
 class B35T(object):
-    #ser,
-    #logFile
-    
+    # ser,
+    # logFile
+
     def __init__(self, port, verbose=False, logFileName=None):
         if verbose:
             log.basicConfig(format='%(asctime)s    %(levelname)s: %(message)s', filename=logFileName, level=log.DEBUG)
             log.info("Verbose output.")
         else:
             log.basicConfig(filename=logFileName)
-        
+
         try:
             self.ser = serial.Serial(
                 port=port,
@@ -276,15 +285,14 @@ class B35T(object):
         else:
             self.serialThread = serial_thread(self.ser)
             self.serialThread.start()
-            
+
     def __del__(self):
-        if not self.ser is None:
+        if self.ser is not None:
             self.serialThread.stop_event.set()
             while self.serialThread.alive:
-                pass #wait for the thread to die
+                pass  # wait for the thread to die
             self.ser.close()
-        
-    
+
     def measure(self, count=3, retries=10):
         '''measures (takes readings until the last [count] don't differ)'''
         log.info('Entered measure')
@@ -294,15 +302,15 @@ class B35T(object):
         last_time = datetime.datetime.now()
         readings = [None] * count
         while not ok:
-            #get reading
-            temp = received_data[-1] #because of the thread
+            # get reading
+            temp = received_data[-1]  # because of the thread
             if temp.dateTime > last_time:
                 readings[i] = temp
                 last_time = datetime.datetime.now()
-                log.debug('measure - New reading (i={}): {}'.format(i, str(temp)))
-                i+= 1    
-            
-            if i >= count : #process the data
+                log.debug('measure - New reading (i = {}): {}'.format(i, str(temp)))
+                i += 1
+
+            if i >= count:  # process the data
                 log.debug('measure - i >= {}'.format(count))
                 readingsMatch = True
                 for r in readings:
@@ -310,24 +318,25 @@ class B35T(object):
                     if not readings[0].matches(r):
                         readingsMatch = False
                         log.debug('measure - Readings do not match')
-                  
-                if readingsMatch: #the last [count] readings are OK
+
+                if readingsMatch:  # the last [count] readings are OK
                     ok = True
                 else:
                     if error_counter >= retries - 1:
-                        log.error('measure - Value is not stable') 
+                        log.error('measure - Value is not stable')
                         raise Exception('Value is not stable')
 
                     error_counter += 1
-                    for j in range(count - 1): readings[j] = readings[j + 1] #shift the array
-                    i-= 1 #take another reading
+                    for j in range(count - 1):
+                        readings[j] = readings[j + 1]  # shift the array
+                    i -= 1  # take another reading
 
         log.info('measure - returned {}'.format(str(readings[-1])))
         return(readings[-1])
-        
+
     def read(self):
         '''Reads a value from the DMM (does not check for unstability and may return old value in case the program freezes)'''
         return(received_data[-1])
-        
+
     def __str__(self):
         return('B35T DMM on port {}'.format(str(self.ser)))
--- B35T_reader/B35T_reader.py
+++ B35T_reader/B35T_reader.py
@@ -1,18 +1,20 @@
+#!/usr/bin/env python
 import sys
 import B35T
 from cmd import Cmd
 import datetime
 import time
 
-#TODO - datetime - do not show milliseconds
+# TODO - datetime - do not show milliseconds
 
 COM_DEFAULT = 'COM19'
+
 
 class MyPrompt(Cmd, object):
     prompt = '\n[disconnected] DMM> '
     logFileName = None
     verbose = False
-    
+
     def do_logfile(self, inp):
         '''Logs errors into a file instead of displaying them. This has to be executed before connect. Argument: filename.extension'''
         if dmm is None and not inp == '':
@@ -37,73 +39,74 @@ class MyPrompt(Cmd, object):
             else:
                 print(inp)
                 port = inp
-            dmm = B35T.B35T(port, verbose = self.verbose, logFileName = self.logFileName)
-            if not dmm.ser is None:
+            dmm = B35T.B35T(port, verbose=self.verbose, logFileName=self.logFileName)
+            if dmm.ser is not None:
                 self.prompt = '\n[connected] DMM> '
             else:
                 dmm = None
         else:
             print('Already connected')
-             
+
     def do_measure(self, inp):
         '''Measure the value and return it as text. Also logs if verbose'''
-        if not dmm is None:
+        if dmm is not None:
             try:
-                print('Measurement: '), 
+                print('Measurement: '),
                 print(str(dmm.measure()))
             except Exception as e:
                 print('Exception: {} occured.'.format(str(e)))
         else:
             print('You have to connect first')
-        
+
     def do_read(self, inp):
         '''Reads a value from the DMM (does not check for unstability and may return old value in case the program freezes)'''
-        if not dmm is None:
+        if dmm is not None:
             try:
                 reading = dmm.read()
-                timediff = datetime.datetime.now() - reading.dateTime            
+                timediff = datetime.datetime.now() - reading.dateTime
                 print('{} ({} ago)'.format(str(reading), timediff))
             except Exception as e:
                 print('Exception: {} occured.'.format(str(e)))
         else:
             print('You have to connect first')
-            
+
     def do_ping(self, inp):
         '''Prints the time elapsed since last reading. Argument: number_of_pings (each ping takes 1 second)'''
-        if not dmm is None:
+        if dmm is not None:
             if inp != '':
                 count = int(inp)
                 if count == 0 or count > 20:
                     print('Please enter a valid count.')
                     count = 0
             else:
-                count = 5 #default
+                count = 5  # default
 
             for i in range(count):
                 try:
                     reading = dmm.read()
-                    timediff = datetime.datetime.now() - reading.dateTime            
+                    timediff = datetime.datetime.now() - reading.dateTime
                     print('{}   The last reading was taken {} ago:   {}'.format(datetime.datetime.now(), timediff, str(reading)))
                 except Exception as e:
                     print('Exception: {} occured.'.format(str(e)))
                 time.sleep(1)
-            print ('Ping completed')
+            print('Ping completed')
         else:
-            print('You have to connect first')   
-        
+            print('You have to connect first')
+
     def do_exit(self, inp):
         '''Exit the application'''
         return (True)
-    
+
     do_EOF = do_exit
     do_quit = do_exit
-        
+
+
 if __name__ == '__main__':
 
     dmm = None
-    
+
     p = MyPrompt()
-    p.cmdloop() #the CLI
+    p.cmdloop()  # the CLI
     print('End')
     del dmm
     sys.exit(1)
