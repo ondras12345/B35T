37992842c4301b3ab586a7aa28a0031aa0caea56
Ondřej Sluka <ondras12345@gmail.com>
2019-07-11 13:22:11 +0200

    Modify linter ignores to match new symlink

--- tests/linters/flake.sh
+++ tests/linters/flake.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 thisDir=$(git rev-parse --show-toplevel)
 cd $thisDir
-flake8 --exclude .git,B35t_reader/B35T.py --max-line-length 119
+flake8 --exclude .git,B35t_reader/B35T --max-line-length 119
 cd - > /dev/null  # go back to previous directory
