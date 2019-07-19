1232d8bab16ab1bbf2bf327224b3b4b3b636e373
Ondřej Sluka <ondras12345@gmail.com>
2019-07-19 10:16:25 +0200

    Fix symlink creation error on Windows
    The error caused the symlink to be crated in C:\Windows\System32
    if the repo was located on a different drive than C
    (cd D:\repo can't change drive, you have to do D: first)

--- B35T_reader/create-symlink.sh
+++ B35T_reader/create-symlink.sh
@@ -3,9 +3,8 @@ repoDir=$(git rev-parse --show-toplevel)
 cd "$repoDir/B35T_reader"
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "Creating windows batch file"
-    echo "cd \"$repoDir/B35T_reader\"" > create-symlink-windows.bat 
-    echo "mklink /D B35T \"$repoDir/B35T\"" >> create-symlink-windows.bat
-    echo "pause" >> create-symlink-windows.bat 
+    echo "mklink /D \"$repoDir/B35T_reader/B35T\" \"$repoDir/B35T\"" >> create-symlink-windows.bat
+    echo "pause" >> create-symlink-windows.bat
     read -p "This script will now exit. Run the .bat file as administartor  "
 else
     ln -s -d "$repoDir/B35T"
